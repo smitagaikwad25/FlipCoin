0c5c07d257ebcd8ee2c2f906d309ce534ea13d03
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-11-19 19:21:34 +0530

    Add : toss to check head or tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,15 @@
 echo "Welcome to flipCoin Simulator"
+
+#constant
+#variables
+random=0
+head=0
+tail=0
+
+	random=$((RANDOM%2))
+	if  [ $random -eq 1 ]
+	then
+		echo "HEAD"
+	else
+		echo "TAIL"
+	fi
