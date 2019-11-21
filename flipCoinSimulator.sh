38a31cdf73e12e613e51f5879368bca43faf47cc
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-11-21 11:20:10 +0530

    Add :Store the double combination and percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,8 +5,11 @@ echo "Welcome to flipCoin Simulator"
 
 #variables
 random=0
-head=0
-tail=0
+HH=0
+HT=0
+TH=0
+TT=0
+
 counter=0
 
 #dictionary
@@ -17,30 +20,52 @@ declare -A flipcoin
 	echo "Enter number to flip coin"
 	read flip 
 
+
 	while [ $counter != $flip ]
 	do
 
-	random=$((RANDOM%2))
-	if  [ $random -eq 1 ]
-        then
-                (( head++ ))
-                echo "HEAD"  
+	random1=$((RANDOM%2))
+	random2=$((RANDOM%2))
 
-        else
-		(( tail++ ))
-		echo "TAIL"
+	if  [[ $random1 -eq 1 && $random2 -eq 1 ]]
+        then
+                (( HH++ ))
+                echo "HEAD HEAD"
+	elif [[ $random1 -eq 1 && $random2 -eq 0 ]]
+	then
+		(( HT++ ))
+               echo  "HEAD TAIL" 
+
+        elif [[ $random1 -eq 0 && $random2 -eq 1 ]]
+	then
+		(( TH++ ))
+		echo "TAIL HEAD "
+	else
+		(( TT++ ))
+		echo "TAIL TAIL"
 	fi
 		(( counter++ ))
 
-		flipcoin[random]="$random"
-	        echo "Singlet Combination" ${flipcoin[@]}
+		flipcoin[11]="HH"
+		flipcoin[10]="HT"
+		flipcoin[01]="TH"
+		flipcoin[00]="TT"
+		flipcoin[]
 
-		
+
+
 
 	done
 
 
-	echo "percentage of head =" $(($head*100/$flip))
-	echo "percentage of tail =" $(($tail*100/$flip))
 
+	echo  percentHH=$(($HH*100/$flip))
+	echo  percentHT=$(($HT*100/$flip))
+	echo  percentTH=$(($TH*100/$flip))
+	echo  percentTT=$(($TT*100/$flip))
+
+	echo "Doublet Combination" ${flipcoin[@]}
+
+
+random=0
 
